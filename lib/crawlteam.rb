e29d022dc6fc82e802d0d1818c1bc0534388d811
mrk <mrk@ghost.intha.night>
2015-09-10 23:41:46 +0200

    main rb now uses lib functions and prints only performance plot

--- lib/crawlteam.rb
+++ lib/crawlteam.rb
@@ -8,27 +8,23 @@ require 'net/http'
 
 def crawlteam
   team = "2479888"
-  event = 0
-  loop do
-    event = event + 1
-    uri = URI.parse("http://es.fantasy.premierleague.com/entry/#{team}/event-history/#{event}/")
-    res = Net::HTTP.get_response(uri)
-    break if res.code == "404"
-  end
-  event = event - 1
+  event = 4
+  a_player = Array.new
+#  loop do
+#    event = event + 1
+#    uri = URI.parse("http://es.fantasy.premierleague.com/entry/#{team}/event-history/#{event}/")
+#    res = Net::HTTP.get_response(uri)
+#    break if res.code == "404"
+#  end
+#  event = event - 1
   page = Nokogiri::HTML(open("http://es.fantasy.premierleague.com/entry/#{team}/event-history/#{event}/"))
-  page.css('div.ismPlayerContainer dl.ismElementDetail dt span.ismElementText.ismPitchWebName.JS_ISM_NAME').text do |playername|
-    #return playername.text
-    player = playername
-    player = player[0...-1]
-    player[0] = ''
-  end.map do |playera|
-    #player.text.trim
-    #player = player.text
-    #player = player[0...-1]
-    #player[0] = ''
-    puts playera.class
+  page.css('div.ismPlayerContainer dl.ismElementDetail dt span.ismElementText.ismPitchWebName.JS_ISM_NAME').each do |playername|
+    playername = playername.text
+    playername = playername[0...-1]
+    playername[0] = ''
+    a_player.insert(-1, playername)
   end
+  return a_player
 end
 
-crawlteam
+
--- main.rb
+++ main.rb
@@ -4,17 +4,10 @@ Dir["./lib/*.rb"].each {|file| require file }
 
 DATA_DIR = "/var/fantasyfungol/data/"
 
-#crawlteam do |player|
-#  puts player
-#  puts worst(player)
-#end
 
 array = crawlteam
 
-puts array.inspect
 
-
-#puts worst(array)
-#puts worst(crawlteam)
+puts worst(array)
 
 
