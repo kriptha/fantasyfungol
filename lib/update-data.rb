5fb84b86d8537fc0557c7ef3d9ec62af2b9233b0
mrk <mrk@ghost.intha.night>
2015-09-11 00:46:42 +0200

    new interactive menu

---
+++ lib/update-data.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'open-uri'
+require 'json'
+
+
+#DATA_DIR="/var/fantasyfungol/data"
+def updatedata
+  Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
+#  BASE_URL = "http://fantasy.premierleague.com/web/api/elements/"
+  (1..600).each do |i|
+    player_url = BASE_URL + "#{i}"
+    puts "Fetching #{player_url}" 
+    begin
+      player_data = JSON.parse(open(player_url).read)
+      player_content = open(player_url).read
+    rescue Exception=>e
+        puts "Error: #{e}"
+        sleep 5
+    else
+      player_name = player_data.fetch("web_name")
+      puts "Player name : #{player_name}"
+      local_fname = "#{DATA_DIR}/#{File.basename(player_name)}"
+      File.open(local_fname, 'w'){|file| file.write(player_content)}
+      puts "\t...Success, saved to #{local_fname}"
+    ensure
+      sleep 1.0 + rand
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 #!/usr/bin/env ruby
 
+require 'highline/import'
 Dir["./lib/*.rb"].each {|file| require file }
 
 DATA_DIR = "/var/fantasyfungol/data/"
+BASE_URL = "http://fantasy.premierleague.com/web/api/elements/"
+
 
 array = crawlteam
-puts worst(array)
+puts "Welcome to Fantasy Fungol stats app! "
+loop do
+  choose do |menu|
+    menu.prompt = "Please choose an action:  "
 
+    menu.choice(:"Worst performing players") { puts worst(array) }
+    menu.choice(:"Update player data (can take up to 10 minutes...) ") { updatedata }
+  end
+  puts ""
+  puts ""
+end
 
