efb23768288893eae0c037293c06355734cb505e
mrk <mrk@ghost.intha.night>
2015-09-18 23:28:17 +0200

    cleaning main.rb

--- main.rb
+++ main.rb
@@ -13,13 +13,12 @@ DATA_DIR = "/var/fantasyfungol/data/"
 BASE_URL = "http://fantasy.premierleague.com/web/api/elements/"
 
 
-array = crawlteam
 puts "Welcome to Fantasy Fungol stats app! "
 loop do
   choose do |menu|
     menu.prompt = "Please choose an action:  "
 
-    menu.choice(:"Worst performing players") { puts worst(array) }
+    menu.choice(:"Worst performing players") { puts worst(crawlteam) }
     menu.choice(:"Update player data (can take up to 10 minutes...) ") { updatedata }
   end
   puts ""
